472a39aeac7add1c793807211583683163b29e4b
Michael Fuget <michaelfuget@gmail.com>
2021-02-02 00:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
-let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader)
 
 newHeader.innerHTML = 'YOUR-NAME is the champion'
